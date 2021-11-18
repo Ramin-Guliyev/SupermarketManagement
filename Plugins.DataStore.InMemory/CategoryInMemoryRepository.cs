d4d50cb72db21e49bac89e166215bb3da80d567f
Ramin-Guliyev <ra1000ishere@outlook.com>
2021-11-19 00:18:01 +0400

    Add project files.

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UseCases.DataStorePlaginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+        public CategoryInMemoryRepository()
+        {
+            categories = new List<Category>()
+            {
+                new Category{CategoryId = 1, Name ="Beverage", Description = "Beverage Desc"},
+                new Category{CategoryId = 2, Name ="Meat", Description = "Meat Desc"},
+                new Category{CategoryId = 3, Name ="Bakery", Description = "Bakery Desc"}
+
+            };
+        }
+
+        public void AddCategory(Category category)
+        {
+            if (categories.Any(x => x.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase)))
+                return;
+            var id = categories.Max(x=>x.CategoryId);
+            category.CategoryId = id + 1;
+            categories.Add(category);
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToUpdate = GetCategoryById(category.CategoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+        public Category GetCategoryById(int categoryId)
+        {
+            return categories?.FirstOrDefault(x => x.CategoryId == categoryId);
+        }
+    }
+}
---
+++ UseCases/ViewCategoriesUseCase.cs
@@ -0,0 +1,22 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using UseCases.DataStorePlaginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases
+{
+    public class ViewCategoriesUseCase : IViewCategoriesUseCase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IEnumerable<Category> Execute()
+        {
+            return _categoryRepository.GetCategories();
+        }
+    }
+}
